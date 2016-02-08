461734c3fe6f217644d35222ad112811aef9c00e
Alexander Scheel <alexander.m.scheel@gmail.com>
2016-02-08 15:38:34 -0600

    Fixed binheap hode-->node

--- src/player.c
+++ src/player.c
@@ -1,18 +1,22 @@
 #include <stdlib.h>
 
-void player_init(map* current) {
+void player_init(map* current)
+{
     int room = rand() % current->room_count;
-    
+
 
     current->character;
 }
 
-void player_distances(map* current) {
+void player_distances(map* current)
+{
 }
 
-void player_wall_distances(map* current) {
+void player_wall_distances(map* current)
+{
 }
 
-void player_entire_distances(map* current) {
+void player_entire_distances(map* current)
+{
 }
 
