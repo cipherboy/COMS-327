acaedae2dc50500ffbe8d6ac5ece5d9adaa625bb
Alexander Scheel <alexander.m.scheel@gmail.com>
2016-02-21 15:57:44 -0600

    Fixed main method; updated README.

--- README.md
+++ README.md
@@ -1,11 +1,26 @@
-# Project 1.03
+# Project 1.04
 
     u: Alexander Scheel
     e: <scheel@iastate.edu>
     l: BSD: 2-clause
 
-The goal of this project is to add path-finding monsters.
+## Overview
+The goal of this part of the project is to add moving monsters and playing
+characters to the game. In particular, there exist four base attributes any
+monster could have:
 
-To compile and run:
+- Intelligence
+- Telepathy
+- Tunneling
+- Erratic Behavior
+
+The probability of having any one of these attributes is 50%. At this stage,
+the player's movement is unable to be controlled and simply wanders around
+aimlessly. Poor guy, almost certain to die a horrible death...
+
+
+## Running
+To compile and run:  
+
     make all
     ./bin/rlg327
--- src/main.c
+++ src/main.c
@@ -19,40 +19,44 @@ int main(int argc, char* argv[])
     }
 
     map r;
-    bool init = false;
+    bool load = false;
+    bool save = false;
+    bool other = false;
     char* path;
     path = malloc(sizeof(char) * 1024);
     snprintf(path, 1024, "%s/.rlg327/dungeon", getenv("HOME"));
 
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "--load") == 0) {
-            printf("Loading...\n");
-            if (!map_read(&r, path)) {
-                map_init(&r);
-            }
-            map_player_init(&r);
-            map_player_distances(&r);
-            map_print(&r);
-            init = true;
-        }
-        if (strcmp(argv[i], "--save") == 0) {
-            printf("Saving...\n");
-            if (init == false) {
-                map_init(&r);
-                map_player_init(&r);
-                map_player_distances(&r);
-                map_print(&r);
-                init = true;
-            }
-            map_write(&r, path);
+            load = true;
+        } else if (strcmp(argv[i], "--save") == 0) {
+            save = true;
+        } else {
+            other = true;
         }
     }
 
-    if (!init) {
+    if ((!load && !save) || other) {
         printf("Usage: %s [--save] [--load]\n\t--save\t- create a new world and save state\n\t--load\t-load an existing world\n", argv[0]);
         map_deinit(&r);
         free(path);
         return 1;
+    }
+
+    if (load) {
+        if (!map_read(&r, path)) {
+            printf("Error reading path: %s. Are you sure it exists and is readable?", path);
+            return 0;
+        }
+    } else {
+        map_init(&r);
+    }
+    map_player_init(&r);
+    map_player_distances(&r);
+    map_print(&r);
+
+    if (save) {
+        map_write(&r, path);
     }
 
 
